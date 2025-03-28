a5c0d3f4fda3fb3c1ebc011e304bd69180f61376
pauchok-sk <pauchoooooook@gmail.com>
2025-03-28 15:04:46 +0300

    prices page

--- dist/js/app.js
+++ dist/js/app.js
@@ -233,6 +233,7 @@
     }
     function scrollables() {
         new Scrollable("#portfolio-nav");
+        new Scrollable("#prices-nav");
     }
     function select_select() {
         const buttons = document.querySelectorAll(".select__button");
--- src/js/files/scrollables.js
+++ src/js/files/scrollables.js
@@ -2,4 +2,5 @@ import Scrollable from "../lib/Scrollable.js";
 
 export default function scrollables() {
   new Scrollable("#portfolio-nav");
+  new Scrollable("#prices-nav");
 }
